7dbb49094d0076e3ca5bd7a0b0f07655ba2ca64b
Minjeong Shin <minjeong.shin@anu.edu.au>
2020-10-14 23:56:48 +0900

    update script with few ranking changing example

--- core/core_utils.py
+++ core/core_utils.py
@@ -21,16 +21,16 @@ def read_core_data():
     df = pd.concat(data, ignore_index = True)
     return df
 
-df = read_core_data()
+# df = read_core_data()
 # print(df)
 # print(df.loc[df["Acronym"] == "WSDM"])
 # print(df.loc[df["Acronym"] == "ICLR"])
 # print(df.loc[df["Acronym"] == "NeurIPS"])
 # print(df.loc[df["FoR1"] == 4610.0])
 
-conf_2020 = df.loc[df["Year"] == "CORE2020"][["Acronym"]].values.tolist()
-conf_2018 = df.loc[df["Year"] == "CORE2018"][["Acronym"]].values.tolist()
+# conf_2020 = df.loc[df["Year"] == "CORE2020"][["Acronym"]].values.tolist()
+# conf_2018 = df.loc[df["Year"] == "CORE2018"][["Acronym"]].values.tolist()
 # print(conf_2014)
-for c in conf_2018:
-    if c not in conf_2020:
-        print("{} is removed in 2020".format(c[0]))
+# for c in conf_2018:
+#     if c not in conf_2020:
+#         print("{} is removed in 2020".format(c[0]))
